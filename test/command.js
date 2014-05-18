2f56d5160f92e4e7410f9c12714ae37fb0b86660
bouzuya <bouzuya@gmail.com>
2014-05-19 07:20:23 +0900

    fix some bugs

--- test/command.js
+++ test/command.js
@@ -2,8 +2,239 @@ var option = require('../lib/option');
 var command = require('../lib/command');
 
 describe('Command', function() {
+  describe('name()', function() {
+    context('command(\'abc\').name()', function() {
+      it('returns \'abc\'', function() {
+        expect(command('abc').name()).to.equal('abc');
+      });
+    });
+
+    context('command().name(\'abc\').name()', function() {
+      it('returns \'abc\'', function() {
+        expect(command().name('abc').name()).to.equal('abc');
+      });
+    });
+  });
+
+  describe('action()', function() {
+    context('command().action(f).action()', function() {
+      it('returns f', function() {
+        var f = function() {};
+        expect(command().action(f).action()).to.equal(f);
+      });
+    });
+  });
+
+  describe('version()', function() {
+    context('command().version(\'0.1.0\').version()', function() {
+      it('returns \'0.1.0\'', function() {
+        expect(command().version('0.1.0').version()).to.equal('0.1.0');
+      });
+    });
+  });
+
+  describe('example()', function() {
+    context('command().example(\'hoge\').example()', function() {
+      it('returns [\'hoge\']', function() {
+        expect(command().example('hoge').example()).to.deep.equal(['hoge']);
+      });
+    });
+  });
+
+  describe('option()', function() {
+    context('command().option(\'--x\', \'y\')', function() {
+      it('returns command', function() {
+        var c = command();
+        expect(c.option('--x', 'y')).to.equal(c);
+      });
+    });
+  });
+
+  describe('command()', function() {
+    context('command().command(\'action\', \'action\')', function() {
+      it('returns subcommand', function() {
+        var c = command()
+        expect(c.command('action', 'action')).to.not.equal(c);
+      });
+    });
+  });
+
+  describe('printVersion()', function() {
+    beforeEach(function() {
+      this.stdout = '';
+      this.sinon.stub(console, 'log', function(s) {
+        this.stdout += s;
+      }.bind(this));
+    });
+
+    it('output the version number', function() {
+      command().version('0.1.0').printVersion();
+      this.sinon.restore();
+      expect(this.stdout).to.equal('0.1.0');
+    });
+  });
+
+  describe('printHelp()', function() {
+    beforeEach(function() {
+      this.stdout = '';
+      this.sinon.stub(console, 'log', function(s) {
+        this.stdout += s;
+      }.bind(this));
+    });
+
+    context('default', function() {
+      it('output help information', function() {
+        command('mycommand').printHelp();
+        this.sinon.restore();
+        var help = [
+          '',
+          '  Usage: mycommand',
+          '',
+          '  Options: ',
+          '',
+          '    -h, --help output usage information',
+          '',
+        ].join('\n');
+        expect(this.stdout).to.equal(help);
+      });
+    });
+
+    context('has version', function() {
+      it('output help information', function() {
+        command('mycommand').version('0.1.0').printHelp();
+        this.sinon.restore();
+        var help = [
+          '',
+          '  Usage: mycommand',
+          '',
+          '  Options: ',
+          '',
+          '    -h, --help    output usage information',
+          '    -V, --version output the version number',
+          '',
+        ].join('\n');
+        expect(this.stdout).to.equal(help);
+      });
+    });
+
+    context('has command', function() {
+      it('output help information', function() {
+        var c = command('mycommand <command>');
+        c.command('subcommand', 'description')
+        c.printHelp();
+        this.sinon.restore();
+        var help = [
+          '',
+          '  Usage: mycommand <command>',
+          '',
+          '  Commands: ',
+          '',
+          '    subcommand description',
+          '',
+          '  Options: ',
+          '',
+          '    -h, --help output usage information',
+          '',
+        ].join('\n');
+        expect(this.stdout).to.equal(help);
+      });
+    });
+
+    context('has example', function() {
+      it('output examples', function() {
+        var c = command('mycommand');
+        c.example('mycommand abc def')
+        c.example('mycommand ghi jkl')
+        c.printHelp();
+        var help = [
+          '',
+          '  Usage: mycommand',
+          '',
+          '  Options: ',
+          '',
+          '    -h, --help output usage information',
+          '',
+          '  Examples: ',
+          '',
+          '    mycommand abc def',
+          '    mycommand ghi jkl',
+          '',
+        ].join('\n');
+        expect(this.stdout).to.equal(help);
+      });
+    });
+  });
+
+  describe('execute()', function() {
+    beforeEach(function() {
+      this.stdout = '';
+      this.exitCode = 0;
+      this.sinon.stub(console, 'log', function(s) {
+        this.stdout += s;
+      }.bind(this));
+      this.sinon.stub(process, 'exit', function(i) {
+        this.exitCode = i;
+      }.bind(this));
+    });
+
+    context('', function() {
+      it('execute action', function() {
+        command()
+        .action(function() { console.log('action'); })
+        .execute(['node', 'index.js']);
+        this.sinon.restore();
+        expect(this.stdout).to.equal('action');
+        expect(this.exitCode).to.equal(0);
+      });
+    });
+
+    context('-h', function() {
+      it('output help information', function() {
+        command()
+        .action(function() { console.log('action'); })
+        .execute(['node', 'index.js', '-h']);
+        this.sinon.restore();
+        var help = [
+          '',
+          '  Usage: index',
+          '',
+          '  Options: ',
+          '',
+          '    -h, --help output usage information',
+          '',
+        ].join('\n');
+        expect(this.stdout).to.equal(help);
+        expect(this.exitCode).to.equal(0);
+      });
+    });
+
+    context('-V', function() {
+      it('output the version number', function() {
+        command()
+        .version('0.1.0')
+        .action(function() { console.log('action'); })
+        .execute(['node', 'index.js', '-V']);
+        this.sinon.restore();
+        expect(this.stdout).to.equal('0.1.0');
+        expect(this.exitCode).to.equal(0);
+      });
+    });
+
+    context('subcommand', function() {
+      it('execute subcommand action', function() {
+        var p = command('command <command>');
+        p.action(function() { console.log('action'); });
+        p.command('subcommand').action(function() { console.log('sub'); });
+        p.execute(['node', 'index.js', 'subcommand']);
+        this.sinon.restore();
+        expect(this.stdout).to.equal('sub');
+        expect(this.exitCode).to.equal(0);
+      });
+    });
+  });
+
   describe('_parseExpectedArgs', function() {
-    beforeEach(function() { this.f = command.prototype._parseExpectedArgs; });
+    beforeEach(function() { this.f = command._parseExpectedArgs; });
 
     context('command', function() {
       it('works', function() {
@@ -60,7 +291,7 @@ describe('Command', function() {
   describe('_parseArgs', function() {
     beforeEach(function() {
       this.f = function(name, args) {
-        return command._parseArgs(new command(name)._args, args);
+        return command._parseArgs(command(name)._args, args);
       };
     });
 
